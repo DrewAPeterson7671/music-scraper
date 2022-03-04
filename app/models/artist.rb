17249b12e6f07b5581d616c4112a91f404800d88
Drew Peterson <drew.a.peterson@hotmail.com>
2022-03-04 15:05:58 -0800

    Add Artist validations to model

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,7 @@
 class Artist < ApplicationRecord
+  validates :name, :genre, :priority, presence: true,
+  validates :name, uniqueness: { message: ': Artist %{value} already exists.' }
+
 end
 
 # NEED TO ELIMINATE BLANKS
