f52aeff5a031248a18b15bdac93241c0582d6712
Drew Peterson <drew.a.peterson@hotmail.com>
2022-03-03 16:18:31 -0800

    billboard methods completed

--- app/helpers/scraper_helper.rb
+++ app/helpers/scraper_helper.rb
@@ -45,7 +45,8 @@ module ScraperHelper
 
     @list_store.each do |ls|
       ls.each do |lsa|
-        lsa.slice! "\r\n      "
+        lsa.gsub!(/\\n|\\t/)
+        # lsa.slice! "\r\n      "
       end
     end
   end
@@ -79,36 +80,89 @@ module ScraperHelper
       @other_countdowns << i.try(:text)
       @other_countdowns << i['href']
     end
-
-    def rocklists_by_year(year)
-      # append station and year to instance to ID it?
-      doc_list = target_scrape("https://www.rocklists.com#{year}")
-      info = doc_list.css('div.entry-content')
-      @year_list = []
-
-      info.css('p').each { |i| @year_list  << i.try(:text) }
-
-      @year_list.map do |yl| 
-        yl.gsub!(/\\n|\\t/)
-        yl.strip
-      end
-
-      @year_list = @year_list.map { |yl| yl.split(/\. | - /) }
-      # J. Geils Band messes this up
-
-      # @year_list.map do |yl| 
-      #   yl.map do |yla|
-      #     yla.gsub!(/\\n|\\t/)
-      #     yla.strip
-      #   end
-      # end
-
-      end
-      # Need to use regex to split up each string into rank, artist, song
-
-    end
-    # other countdowns are not homogenous and may need special handling by station
   end
+
+  def rocklists_by_year(year)
+    # append station and year to instance to ID it?
+    doc_list = target_scrape("https://www.rocklists.com#{year}")
+    info = doc_list.css('div.entry-content')
+    @year_list = []
+
+    info.css('p').each { |i| @year_list  << i.try(:text) }
+    
+    @year_list = @year_list.map { |yl| yl.split(/\. | - /) }
+  
+    @year_list = @year_list.map do |yl| 
+      yl.map do |yla|
+        yla.gsub!(/\\n|\\t/, "")
+        yla.strip
+      end
+    end
+  end
+
+  def frankspage(year)
+    # includes more songs for 2010 to 2018, no albums
+    doc_list = target_scrape("http://www.frankspage.net/kroq/#{year}")
+    info = doc_list.css('table.kroq')
+    @year_list = []
+    rows = info.css("tr")
+
+    @year_list = rows.css('tr').map { |tr| tr.css('td').map &:text}
+    @year_list.delete_if(&:blank?)
+
+    @year_list = @year_list.map do |yl| 
+      yl.map do |yla|
+        yla.gsub!("\n", "")
+        yla.gsub!("\t", "")
+        yla.strip
+      end
+    end
+  end
+
+  def billboardtop100_to_1958(year)
+    # 1940 to 1958
+    if year = "1940" 
+      year = "336"
+    end
+
+    doc_list = target_scrape("http://billboardtop100of.com/#{year}-2")
+
+    info = doc_list.css('div.entry-content')
+    @year_list = []
+
+    rows = info.css("p")
+    info.css('p').each { |i| @year_list  << i.try(:text) }
+    @year_list = @year_list[0].split("\n")
+    @year_list = @year_list2.map { |yl| yl.split(/\. | – /) }
+  end
+
+  def billboard_wiki(year)
+    # from 1959 to 2021
+    doc_list = target_scrape("https://en.wikipedia.org/wiki/Billboard_Year-End_Hot_100_singles_of_#{year}")
+    info = doc_list.css('table.wikitable')
+    @year_list = []
+    rows = info.css("tr")
+
+    @year_list = rows.css('tr').map { |tr| tr.css('td').map &:text}
+
+    @year_list.map do |yl|
+      yl.map do |yla|
+        yla.gsub!("\"", "")
+        yla.gsub!("\n", "")
+      end
+    end
+    # yl[1].gsub!("\"", "")
+    # yl[2].gsub!("\n", "")
+
+    
+
+
+
+  end
+
+    # other countdowns are not homogenous and may need special handling by station
 
 end
 
+# @year_list.each { |yl| yl.gsub!("\n\n\t", "") }
+# @year_list.each { |yl| yl.gsub!("\n\n\n", "") }
