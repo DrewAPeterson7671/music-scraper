4c97eb9161a2d0657d0ad9aa4134264da909dbd0
Drew Peterson <drew.a.peterson@hotmail.com>
2022-07-04 09:13:59 -0700

    add foundation for placeholder on home page with table, user_id index, controller and model

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,52 @@
 class WelcomeController < ApplicationController
-  skip_before_action :authenticate_user!, :only => [:index]
+  skip_before_action :authenticate_user!, :only => [:index, :new, :create, :edit, :update, :destroy]
 
   def index
+    @placeholder = Welcome.all
   end
+
+  def new
+    @placeholder = Welcome.new
+  end
+
+  def edit
+  end
+
+  def create
+    @placeholder = Welcome.new(welcome_params.merge(:user_id => current_user.id))
+
+    respond_to do |format|
+      if @placeholder.save 
+        format.html {redirect to home_url(@placeholder), notice: "Placeholder updated"}
+      else
+        format.html { render :index, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @placeholder.update(welcome_params) 
+        format.html {redirect to home_url(@placeholder), notice: "Placeholder updated"}
+      else
+        format.html { render :index, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @artist.destroy
+
+    respond_to do |format|
+      format.html {redirect to home_url(@placeholder), notice: "Placeholder deleted"}
+    end
+  end
+
+private
+
+  def welcome_params
+    params.require(:welcome).permit(:user_id, :placeholder)
+
+  end
+
 end
