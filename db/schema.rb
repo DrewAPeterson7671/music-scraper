45848291b5874bbe8761c35acd17de1e92ccbf78
Drew Peterson <drew.a.peterson@hotmail.com>
2022-10-05 20:31:26 -0700

    Add will_paginate gem and apply it to all indexes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_23_032246) do
+ActiveRecord::Schema.define(version: 2022_10_05_064145) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 2022_09_23_032246) do
     t.string "rank_album"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "rank_listened"
+    t.string "rank_genre"
   end
 
   create_table "artists", force: :cascade do |t|
