1d9757581636ac22a34a70fb2f28b2edac54d79d
Drew Peterson <drew.a.peterson@hotmail.com>
2022-10-27 00:27:18 -0700

    refactor to create form for index for alt_collection, added alt_collection to views.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@ Rails.application.routes.draw do
   root to: 'welcomes#index'
 
   resources :annual_ranks
+
+  # need to research
+  # map.resources :annual_ranks, :collection => { :alt_collection => :put }
+
+  match 'annual_ranks/all/edit' => 'annual_ranks#edit_all', :as => :edit_all, :via => :get
+  match 'annual_ranks/all' => 'annual_ranks#update_all', :as => :update_all, :via => :put 
+
   devise_for :users
 
   resource :welcome, only: [:new, :update]
